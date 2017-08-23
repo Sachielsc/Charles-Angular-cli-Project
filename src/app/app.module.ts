a6625bfa816ce428506720197a8756e50b3cc80a
Sachielsc <sachielsc@gmail.com>
2017-08-23 14:11:53 +1200

    Wednesday 1st

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
+// use this module to bundle pieces together
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ServerComponent } from './server/server.component';
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+import { ReportComponent } from './report/report.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    TestComponent,
+    ReportComponent
   ],
   imports: [
     BrowserModule
